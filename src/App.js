88a88f161d0ff6c011194ff5eefa7cdfdf7852a3
Aaqib <aaqib.hyd@gmail.com>
2018-09-19 15:08:16 -0300

    mathjs added

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
     this.flagValue = false;
   };
 
+  
   addtoInputMath = (val) => {
     let temp = { input: this.state.input + val };
     let lastDigit = (temp.input.toString().slice(-1));
